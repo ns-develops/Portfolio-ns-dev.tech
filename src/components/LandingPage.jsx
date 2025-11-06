3dc617a4874f1b3899f3525ec82d55d873377f14
”ns-develops” <test.tt9061030@gmail.com>
2025-11-06 21:36:47 +0100

    upd space

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,34 +1,68 @@
-// import React from 'react'
+
+
+function LandingPage() {
+  return (
+    <div className="w-full h-screen bg-zinc-900 pt-1">
+      <div className="px-20 textstructure mt-10 pt-20">
+        
+     
+        <div className="space-y-6">
+          <div className="masker font-[Founders_Grotesk] font-semibold">
+            <h1 className="uppercase text-white text-9xl leading-[5.5vw] tracking-tighter">
+              NS DEV
+            </h1>
+          </div>
+
+          <div className="masker font-[Founders_Grotesk] font-semibold">
+            <h1 className="uppercase text-white text-9xl leading-[5.5vw] tracking-tighter">
+              We Create
+            </h1>
+          </div>
+
+          <div className="masker font-[Founders_Grotesk] font-semibold">
+            <h1 className="uppercase text-white text-9xl leading-[5.5vw] tracking-tighter">
+              Signature
+            </h1>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default LandingPage
+
+{/*import React from 'react'
 
 function LandingPage() {
   return (
 <div className="w-full h-screen bg-zinc-900 pt-1">
-  <div className="px-20 textstructure mt-10 pt-20">
-    <div className="masker font-[Founders_Grotesk] font-semibold">
-      <h1 className="uppercase text-white text-9xl leading-none tracking-tighter">
+  <div className="px-20 pt-20 textstructure mt-10 
+                 uppercase text-white leading-[6.5vw] tracking-tighter 
+                 font-['Founders_Grotesk'] font-semibold"> 
+    
+    <div className="masker mb-2">
+      <h1 className="text-9xl mb-0">
         NS DEV
       </h1>
     </div>
-    <div className="masker font-[Founders_Grotesk] font-semibold">
-      <h1 className="uppercase text-white text-9xl leading-none tracking-tighter">
-        We Create
+    
+    <div className="masker mb-2">
+      <h1 className="text-9xl mb-0">
+        WE CREATE
       </h1>
    </div>
-   <div className="masker font-[Founders_Grotesk] font-semibold">
-      <h1 className="uppercase text-white text-9xl leading-none tracking-tighter">
-        Signature
+   
+   <div className="masker mb-2">
+      <h1 className="text-9xl mb-0">
+        SIGNATURE
       </h1>
    </div>
   </div>
 </div>
 
   )
-}
-
-export default LandingPage
-
-
-
+}}
 {/*import React from 'react'
 
 function LandingPage() {
