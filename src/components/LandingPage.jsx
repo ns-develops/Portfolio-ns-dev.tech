73d3084803f0b0cb5a8158980a6ccf1b0ebd2a2c
”ns-develops” <test.tt9061030@gmail.com>
2025-11-06 23:24:00 +0100

    adding small details

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,25 +1,34 @@
+import React from 'react'
+
 function LandingPage() {
+  const lines = ["NS DEV", "We Create", "Signature"]
+  const subtitles = ["Empowering startups and enterprise-level businesses", "Code meets solution"]
+
   return (
     <div className="w-full h-screen bg-white pt-1">
       <div className="px-20 textstructure mt-10 pt-20">
+
         <div className="space-y-6">
-          <div className="masker font-[Founders_Grotesk] font-semibold">
-            <h1 className="uppercase text-black text-9xl leading-[5.5vw] tracking-tighter">
-              NS DEV
-            </h1>
-          </div>
+          {lines.map((item, index) => (
+            <div key={index} className="masker font-[Founders_Grotesk] font-semibold">
+              <h1 className="uppercase text-black text-9xl leading-[5.5vw] tracking-tighter">
+                {item}
+              </h1>
+            </div>
+          ))}
+        </div>
 
-          <div className="masker font-[Founders_Grotesk] font-semibold">
-            <h1 className="uppercase text-black text-9xl leading-[5.5vw] tracking-tighter">
-              We Create
-            </h1>
-          </div>
+        <div className="border-t-[1px] border-zinc-800 mt-32 "></div>
 
-          <div className="masker font-[Founders_Grotesk] font-semibold">
-            <h1 className="uppercase text-black text-9xl leading-[5.5vw] tracking-tighter">
-              Signature
-            </h1>
-          </div>
+        <div className="flex justify-between mt-4">
+          {subtitles.map((item, index) => (
+            <p
+              key={index}
+              className="text-md font-light tracking-tight leading-none text-zinc-700"
+            >
+              {item}
+            </p>
+          ))}
         </div>
       </div>
     </div>
