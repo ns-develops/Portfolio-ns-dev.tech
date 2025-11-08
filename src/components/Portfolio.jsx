3ff2ff3bf874f4a8c55ea78d47756ed97026c169
”ns-develops” <test.tt9061030@gmail.com>
2025-11-08 21:55:47 +0100

    added iages to featured projects, first draft

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,27 +1,36 @@
 import React from 'react';
+import Fotboll from '../assets/bildfotboll.png';
+import Ecommerce from '../assets/ecommerce2.png';
 
 const Portfolio = () => {
   return (
     <div className="w-full bg-zinc-900 text-white min-h-screen">
       
-      {/* Header */}
       <div className="w-full px-20 border-b-[1px] border-zinc-800 pb-20">
         <h1 className='text-8xl font-["Neue_Montreal"] tracking-tight'>
           Featured projects
         </h1>
       </div>
 
-      {/* Project Cards */}
       <div className="px-20 mt-10">
         <div className="cards w-full flex gap-10">
           
-          <div className="cardcontainer rounded-xl w-1/2 h-[75vh] overflow-hidden">
-            <div className="card w-full h-full rounded-xl bg-green-600"></div>
-           {/* <img className="w-full h-full" src="" alt""/>*/}
+          {/* Ljus box med centrerad bild */}
+          <div className="cardcontainer rounded-xl w-1/2 h-[75vh] overflow-hidden bg-gray-300 flex items-center justify-center">
+            <img 
+              src={Fotboll} 
+              alt="Fotboll" 
+              className="w-6/12 h-6/12 object-cover rounded-xl shadow-lg"
+            />
           </div>
 
-          <div className="cardcontainer rounded-xl w-1/2 h-[75vh] overflow-hidden">
-            <div className="card w-full h-full rounded-xl bg-green-600"></div>
+          {/* Svart box med centrerad Ecommerce-bild */}
+          <div className="cardcontainer rounded-xl w-1/2 h-[75vh] overflow-hidden bg-black flex items-center justify-center">
+            <img 
+              src={Ecommerce} 
+              alt="Ecommerce" 
+              className="w-6/12 h-6/12 object-cover shadow-lg"
+            />
           </div>
 
         </div>
@@ -31,4 +40,4 @@ const Portfolio = () => {
 }
 
 export default Portfolio;
- 
+
