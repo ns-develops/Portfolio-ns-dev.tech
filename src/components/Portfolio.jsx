a8ef73a0a97224857dd504335f86ce0a299ff757
”ns-develops” <test.tt9061030@gmail.com>
2025-11-08 22:35:18 +0100

    trying new letters

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,21 +1,20 @@
 import React from 'react';
 import Fotboll from '../assets/bildfotboll.png';
-import Ecommerce from '../assets/ecommerce2.png';
+import Ecommerce from '../assets/ecommerce1.png';
 
 const Portfolio = () => {
   return (
     <div className="w-full bg-zinc-900 text-white min-h-screen">
       
       <div className="w-full px-20 border-b-[1px] border-zinc-800 pb-20">
-        <h1 className='text-8xl font-["Neue_Montreal"] tracking-tight'>
+        <h1 className="text-8xl font-semibold['Neue_Montreal'] tracking-tight">
           Featured projects
         </h1>
       </div>
 
-      <div className="px-20 mt-10">
-        <div className="cards w-full flex gap-10">
+      <div className="px-20 mt-10 relative">
+        <div className="cards w-full flex gap-10 relative">
           
-          {/* Ljus box med centrerad bild */}
           <div className="cardcontainer rounded-xl w-1/2 h-[75vh] overflow-hidden bg-gray-300 flex items-center justify-center">
             <img 
               src={Fotboll} 
@@ -24,7 +23,7 @@ const Portfolio = () => {
             />
           </div>
 
-          {/* Svart box med centrerad Ecommerce-bild */}
+     
           <div className="cardcontainer rounded-xl w-1/2 h-[75vh] overflow-hidden bg-black flex items-center justify-center">
             <img 
               src={Ecommerce} 
@@ -32,6 +31,11 @@ const Portfolio = () => {
               className="w-6/12 h-6/12 object-cover shadow-lg"
             />
           </div>
+
+       
+          <h1 className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 text-6xl font-bold text-white">
+            FYDE
+          </h1>
 
         </div>
       </div>
