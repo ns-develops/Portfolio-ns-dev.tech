7d86ee1321bbb4757204cff067358035a5cdd53f
”ns-develops” <test.tt9061030@gmail.com>
2025-11-09 18:15:57 +0100

    small animation fix

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,16 +11,26 @@ export default function Navbar() {
   };
 
   return (
-    <nav className="fixed z-[999] w-full px-20 py-8 font-neuemontreal text-black flex justify-between items-center">
-      <div className="logo"></div>
+    <nav className="fixed z-[999] w-full px-20 py-8 font-neuemontreal text-black flex justify-end items-center">
       <div className="links flex gap-10">
         {items.map((item, index) => (
           <button
             key={index}
             onClick={() => scrollToSection(item)}
-            className="text-lg capitalize font-light"
+            className={`relative text-lg capitalize font-light group overflow-hidden ${
+              item === "Contact" ? "ml-16" : ""
+            }`}
           >
-            {item}
+      
+            <span className="block transition-transform duration-300 group-hover:-translate-y-full">
+              {item}
+            </span>
+
+            <span className="absolute left-0 top-full block transition-transform duration-300 group-hover:-translate-y-full">
+              {item}
+            </span>
+
+            <span className="absolute bottom-0 left-0 w-0 h-[1px] bg-black transition-all duration-300 group-hover:w-full"></span>
           </button>
         ))}
       </div>
